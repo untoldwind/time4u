6a49d32c7a3d8e638672724d9611d89ff650cdc8
junglas <junglas@d1e209a9-466a-4101-bc37-f2a0db2545eb>
2008-09-25 11:43:57 +0000

    Orig rev 31
    git-svn-id: https://time4u.svn.sourceforge.net/svnroot/time4u/overhaul/trunk@36 d1e209a9-466a-4101-bc37-f2a0db2545eb

---
+++ client/store/src/main/java/de/objectcode/time4u/client/store/api/event/ProjectRepositoryEvent.java
@@ -0,0 +1,15 @@
+package de.objectcode.time4u.client.store.api.event;
+
+import de.objectcode.time4u.server.api.data.Project;
+
+public class ProjectRepositoryEvent extends RepositoryEvent
+{
+  final Project m_project;
+
+  public ProjectRepositoryEvent(final Project project)
+  {
+    super();
+    m_project = project;
+  }
+
+}
