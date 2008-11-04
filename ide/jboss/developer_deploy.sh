386fd0c01b898954c44613d97e8bb2f2108a429b
junglas <junglas@d1e209a9-466a-4101-bc37-f2a0db2545eb>
2008-11-04 12:21:47 +0000

    Exploded developer deployment
    git-svn-id: https://time4u.svn.sourceforge.net/svnroot/time4u/trunk@225 d1e209a9-466a-4101-bc37-f2a0db2545eb

---
+++ ide/jboss/developer_deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ "$JBOSS_HOME" = "" ]
+then
+  echo "JBOSS_HOME not set"
+  exit 1
+fi
+
+if [ "$JBOSS_CONF" = "" ]
+then
+  echo "JBOSS_CONF not set"
+  exit 1
+fi
+
+CURRENT=`pwd` 
+
+cd $JBOSS_HOME/server/$JBOSS_CONF/deploy
+rm -rf time4u-ear.ear
+mkdir time4u-ear.ear
+cd time4u-ear.ear
+
+
+jar xf $CURRENT/../../assemblies/server-ear/target/time4u-assemblies-ear-*.ear
+
+WEB_UI=`ls time4u-server-web-ui-*.war`
+
+rm -rf $WEB_UI
+ln -s $CURRENT/../../server/web/ui/target/${WEB_UI%.war} $WEB_UI
+
+WEB_WS=`ls time4u-server-web-ws-*.war`
+
+rm -rf $WEB_WS
+ln -s $CURRENT/../../server/web/ws/target/${WEB_WS%.war} $WEB_WS
+
+ENTITIES=`ls time4u-server-entities-*.jar`
+
+rm -rf $ENTITIES
+ln -s $CURRENT/../../server/entities/target/$ENTITIES $ENTITIES
+
+EJB=`ls time4u-server-ejb-*.jar`
+
+rm -rf $EJB
+ln -s $CURRENT/../../server/ejb/target/$EJB $EJB
+
+JAAS=`ls time4u-server-jaas-*.sar`
+
+rm -rf $JAAS
+ln -s $CURRENT/../../server/jaas/target/$JAAS $JAAS
+
+cd $CURRENT
