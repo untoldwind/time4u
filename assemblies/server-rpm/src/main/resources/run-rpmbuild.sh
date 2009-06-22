c78f2cf43223b8a029809fe726f76467bebc1661
junglas <junglas@d1e209a9-466a-4101-bc37-f2a0db2545eb>
2009-06-22 07:50:15 +0000

    Minor fix
    git-svn-id: https://time4u.svn.sourceforge.net/svnroot/time4u/trunk@387 d1e209a9-466a-4101-bc37-f2a0db2545eb

--- assemblies/server-rpm/src/main/resources/run-rpmbuild.sh
+++ assemblies/server-rpm/src/main/resources/run-rpmbuild.sh
@@ -6,7 +6,8 @@ mkdir -p $RPM_TOPDIR/BUILD
 mkdir -p $RPM_TOPDIR/RPMS
 cp ~/jboss-*.zip $RPM_TOPDIR/SOURCES
 
-rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS//time4u-server.spec
-rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS//time4u-server-mysql.spec
-rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS//time4u-server-postgres.spec
-rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS//time4u-server-apache.spec
+rm -rf /tmp/time4u*
+rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS/time4u-server.spec
+rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS/time4u-server-mysql.spec
+rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS/time4u-server-postgres.spec
+rpmbuild --define "_topdir $RPM_TOPDIR" -bb $RPM_TOPDIR/SPECS/time4u-server-apache.spec
