7182b1644f2b587273fd0b2e1a5c8bef46f707df
junglas <junglas@d1e209a9-466a-4101-bc37-f2a0db2545eb>
2009-11-26 16:17:11 +0000

    Experimental bundle.info generation
    git-svn-id: https://time4u.svn.sourceforge.net/svnroot/time4u/trunk@463 d1e209a9-466a-4101-bc37-f2a0db2545eb

--- assemblies/client-mac-x86_64/src/main/groovy/genbundles.groovy
+++ assemblies/client-mac-x86_64/src/main/groovy/genbundles.groovy
@@ -4,27 +4,50 @@ def dir = new File(project.basedir, "target/gen/configuration/org.eclipse.equino
 
 ant.mkdir(dir: dir);
 
+def dependencyMap = new HashMap();
+
+for ( dep in project.dependencies ) {  
+  dependencyMap.put(dep.groupId + ":" + dep.artifactId + (dep.classifier == null ? "" : ":jar:" + dep.classifier), 
+    dep);
+}
+
+
+def bundles = new XmlParser().parse(new File(project.basedir, "src/main/assembly/bundles.xml"));
+def lines = new ArrayList();
+
+for (bundle in bundles.dependencySet.bundle ) {
+	def startLevel = bundle.'@start-level' != null ? bundle.'@start-level' : 4;
+	def autoStart = bundle.'@auto-start' != null ? bundle.'@auto-start' : false;
+	def id = dependencyMap[bundle.text()].groupId + "." + dependencyMap[bundle.text()].artifactId;
+	def version = dependencyMap[bundle.text()].version;
+	
+	lines.add(id + "," + version + ",plugins/" + id + "_" + version + ".jar," + startLevel + "," + autoStart);
+}
+
+for (bundle in bundles.manual.bundle ) {
+	def startLevel = bundle.'@start-level' != null ? bundle.'@start-level' : 4;
+	def autoStart = bundle.'@auto-start' != null ? bundle.'@auto-start' : false;
+	def id = bundle.text();
+	def version = bundle.'@version';
+	def path = bundle.'@path';
+
+	if ( path ) {
+		lines.add(id + "," + version + ",plugins/" + id + "_" + version + "/," + startLevel + "," + autoStart);
+	} else {
+		lines.add(id + "," + version + ",plugins/" + id + "_" + version + ".jar," + startLevel + "," + autoStart);
+	}
+}
+
+lines.sort();
+
 def writer = new PrintWriter(new FileWriter(new File(dir, "bundles.info")));
 
 writer.println("#version=1");
 
-for ( dep in project.dependencies ) {
-  writer.print(dep.groupId);
-  writer.print(".");
-  writer.print(dep.artifactId);
-  writer.print(",");
-  writer.print(dep.version);
-  writer.print(",plugins/");
-  writer.print(dep.groupId);
-  writer.print(".");
-  writer.print(dep.artifactId);
-  writer.print("_");
-  writer.print(dep.version);
-  writer.print(".jar");
-  writer.println(",4,false");
-  
-  println dep.groupId + "." + dep.artifactId + "," + dep.version + ",";
+for ( line in lines ) {
+	writer.println(line);
 }
 
 writer.flush();
 writer.close();
+
