13062d1e9fce10142596dc85d018f3a4e504ecfd
Untoldwind <landru@untoldwind.net>
2009-12-23 14:46:17 +0100

    Added real model

---
+++ server/web/gwt/src/main/java/de/objectcode/time4u/server/web/gwt/client/MainPage.java
@@ -0,0 +1,12 @@
+package de.objectcode.time4u.server.web.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MainPage implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
